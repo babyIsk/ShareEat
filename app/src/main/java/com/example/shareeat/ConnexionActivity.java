9f0c5fbad32527cbf449b3df1804edb80ad35748
babyIsk <115020880+babyIsk@users.noreply.github.com>
2023-10-25 17:58:24 +0200

    Initial commit

---
+++ app/src/main/java/com/example/shareeat/ConnexionActivity.java
@@ -0,0 +1,15 @@
+package com.example.shareeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConnexionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connexion);
+        getSupportActionBar().hide();
+    }
+
+}
