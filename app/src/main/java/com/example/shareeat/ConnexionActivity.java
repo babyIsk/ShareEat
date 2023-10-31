1512eecdae3d19c07e3b37d50e69bf510f155823
babyIsk <115020880+babyIsk@users.noreply.github.com>
2023-10-31 23:44:38 +0100

    test commit branche bab

--- app/src/main/java/com/example/shareeat/ConnexionActivity.java
+++ app/src/main/java/com/example/shareeat/ConnexionActivity.java
@@ -9,7 +9,7 @@ public class ConnexionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
-        getSupportActionBar().hide();
+        getSupportActionBar().hide(); // test babylone
     }
 
 }
