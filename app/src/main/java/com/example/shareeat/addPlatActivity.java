5e5730b0e62fbe9fc2351e4f6b485298203232bc
babyIsk <115020880+babyIsk@users.noreply.github.com>
2023-11-14 22:39:16 +0100

    Création formulaire d'ajout d'un plat (ajouter image du plat avec librairie GitHub ImagePicker + style encadré carré et rond)

--- app/src/main/java/com/example/shareeat/addPlatActivity.java
+++ app/src/main/java/com/example/shareeat/addPlatActivity.java
@@ -1,42 +1,70 @@
 package com.example.shareeat;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class addPlatActivity extends AppCompatActivity {
-    int idP;
-    String titreP;
-    String descriptionP;
-    String[] ingrédients;
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-    public int getIdP() {
-        return idP;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class addPlatActivity extends Activity {
+    ImageView imgP;
+    EditText dateAjout;
+    FloatingActionButton btnAddPhoto;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Lier le layout à l'activity
+        setContentView(R.layout.activity_add_plat);
+
+        //Obtention des références sur les composants (ressources)
+        dateAjout = (EditText) findViewById(R.id.dateFormAjoutPlat);
+        imgP = (ImageView) findViewById(R.id.addImgPlat);
+        btnAddPhoto = (FloatingActionButton) findViewById(R.id.btnAddPhoto);
+
+        btnAddPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            // Utilisation de la librairie ImagePicker
+            public void onClick(View view) {
+                ImagePicker.with(addPlatActivity.this)
+                        .crop()	    			//Crop image(Optional), Check Customization for more option
+                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+            }
+        });
     }
 
-    public void setIdP(int idP) {
-        this.idP = idP;
+    @Override
+    // Sélection d'une image à partir de la galerie (utilisation ImagePicker librairie).
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Uri uri = data.getData();
+        // L'image sélectionnée par l'utilisateur dans une autre activité (ici la galerie d'images)
+        // est affichée dans la vue image (imgP).
+        imgP.setImageURI(uri);
     }
 
-    public String getTitreP() {
-        return titreP;
-    }
+    @Override
+    protected void onResume () {
+        super.onResume();
 
-    public void setTitreP(String titreP) {
-        this.titreP = titreP;
-    }
-
-    public String getDescriptionP() {
-        return descriptionP;
-    }
-
-    public void setDescriptionP(String descriptionP) {
-        this.descriptionP = descriptionP;
-    }
-
-    public String[] getIngrédients() {
-        return ingrédients;
-    }
-
-    public void setIngrédients(String[] ingrédients) {
-        this.ingrédients = ingrédients;
+        // Obtenir la date actuelle
+        Date dateDuJour = new Date();
+        // Formater la date pour l'afficher dans le champ dateAjout
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String dateStr = dateFormat.format(dateDuJour);
+        dateAjout.setText(dateStr);
     }
 }
