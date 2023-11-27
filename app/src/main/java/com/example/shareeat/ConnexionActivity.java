8f0b3cdbb71f7c9cd7039b4740961bf1835bff5e
calli77 <calliclesbazolo@gmail.com>
2023-11-27 18:17:17 +0100

    Page inscription avec animation

--- app/src/main/java/com/example/shareeat/ConnexionActivity.java
+++ app/src/main/java/com/example/shareeat/ConnexionActivity.java
@@ -1,15 +1,31 @@
 package com.example.shareeat;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ConnexionActivity extends AppCompatActivity {
+    private TextView inscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
         getSupportActionBar().hide();
+
+        inscription = findViewById(R.id.cliquezIciTextView);
+
+        inscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent main = new Intent(getApplicationContext(), InscriptionActivity.class);
+                startActivity(main);
+                finish();
+            }
+        });
     }
 
 }
