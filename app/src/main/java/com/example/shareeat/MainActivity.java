99b8061b48eba3b7a82ae9d31e2cdbf54996c1a4
babyIsk <115020880+babyIsk@users.noreply.github.com>
2023-11-29 01:21:24 +0100

    formulaire ajout d'un plat presque complété (RecyclerView, CardView)! + création composant d'un élément (ingrédient), et composant ajout d'un nouvel ingrédient + mainActivity dure 2 sec et affiche le logo (sans animations pr l'instant)

--- app/src/main/java/com/example/shareeat/MainActivity.java
+++ app/src/main/java/com/example/shareeat/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.shareeat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        // Référence à l'ImageView
+        ImageView logoImageView = findViewById(R.id.logoImageView);
+
+        // Animation de l'ImageView
+        logoImageView.animate().translationY(-100f).setDuration(1000).start();
+
+        // Handler pour déclencher une action après un délai
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Transition vers l'activité principale
+                startActivity(new Intent(MainActivity.this, ConnexionActivity.class));
+                // ou masquage du layout temporaire : findViewById(R.id.yourLayoutId).setVisibility(View.GONE);
+            }
+        }, 2000); // Délai de 2000 millisecondes (2 secondes)
     }
+
 }
