21a137efb339d2e5c865665c8d1781a295f8be4c
babyIsk <115020880+babyIsk@users.noreply.github.com>
2023-12-13 02:03:25 +0100

    Ajout modifications + fichiers necessaires pour la liste des ingrédients (lorsqu'on post un plat et qu'on renseigne les ingrédients).

--- app/src/main/java/com/example/shareeat/AddPlatActivity.java
+++ app/src/main/java/com/example/shareeat/AddPlatActivity.java
@@ -4,39 +4,86 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.shareeat.adapter.IngrAdapter;
+import com.example.shareeat.modele.Ingredient;
 import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
-public class addPlatActivity extends Activity {
+public class AddPlatActivity extends Activity {
     ImageView imgP;
     EditText dateAjout;
     FloatingActionButton btnAddPhoto;
+    private RecyclerView ingrRecyclerView;
+    private IngrAdapter ingrAdapter;
+
+    private List<Ingredient> ingredientList;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //Lier le layout à l'activity
+        // Lier le layout à l'activity
         setContentView(R.layout.activity_add_plat);
 
-        //Obtention des références sur les composants (ressources)
+        ingredientList = new ArrayList<>();
+
+        // Obtention des références sur les composants (ressources)
         dateAjout = (EditText) findViewById(R.id.dateFormAjoutPlat);
         imgP = (ImageView) findViewById(R.id.addImgPlat);
         btnAddPhoto = (FloatingActionButton) findViewById(R.id.btnAddPhoto);
+
+        //**************Partie liste des ingrédients ************************************
+        ingrRecyclerView = (RecyclerView) findViewById(R.id.listIngrRecyclerView);
+
+        // LayoutManager est responsable de la mesure et du positionnement
+        // des éléments de la RecyclerView.
+        // Nous devons attacher un layoutManager avant de pouvoir utiliser la RecyclerView.
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        ingrRecyclerView.setLayoutManager(layoutManager);
+
+        ingrAdapter = new IngrAdapter(this, ingredientList);
+        ingrRecyclerView.setAdapter(ingrAdapter);
+
+        Ingredient ingr = new Ingredient();
+        ingr.setIngr("C'est un ingredient test");
+        ingr.setStatus(0); // 0 -> false, 1 -> true
+        ingr.setId(1);
+
+        ingredientList.add(ingr);
+        ingredientList.add(ingr);
+        ingredientList.add(ingr);
+        ingredientList.add(ingr);
+        ingredientList.add(ingr);
+        ingredientList.add(ingr);
+
+        if (ingredientList.isEmpty()) {
+            // La liste d'ingrédients est vide, ajoutez des logs ou un message de débogage.
+            Log.d("AddPlatActivity", "La liste d'ingrédients est vide.");
+        } else {
+            // La liste d'ingrédients contient des éléments, mettez à jour l'adaptateur.
+            ingrAdapter.setIngredients(ingredientList);
+        }
+        //************** FIN (liste des ingrédients) ************************************
 
         btnAddPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             // Utilisation de la librairie ImagePicker
             public void onClick(View view) {
-                ImagePicker.with(addPlatActivity.this)
+                ImagePicker.with(AddPlatActivity.this)
                         .crop()	    			//Crop image(Optional), Check Customization for more option
                         .compress(1024)			//Final image size will be less than 1 MB(Optional)
                         .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
---
+++ app/src/main/java/com/example/shareeat/modele/Ingredient.java
@@ -0,0 +1,32 @@
+package com.example.shareeat.modele;
+
+public class Ingredient {
+
+    int id;
+    int status;
+    String ingr;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getIngr() {
+        return ingr;
+    }
+
+    public void setIngr(String ingr) {
+        this.ingr = ingr;
+    }
+}
